a7277350344e51a6e00d399a8e9b622f7adabb18
Nadavse <nadavse@gmail.com>
2019-02-19 20:01:38 +0200

    Update Imagetestjqcode.js

--- Imagetestjqcode.js
+++ Imagetestjqcode.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
           };
         
         // Change texts by gender
-        if (currentGender == "male" || currentGender == "female") {
+        if (currentGender == "male") {
         
             $("#title-part-2").text("האישיות העיצובית של " + currentName);
             $("#successPartTwoTitle").text(" נהדר, סיימת את החלק שלך בהצלחה!" + currentName);
